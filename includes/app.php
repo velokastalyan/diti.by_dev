02c0d80b26801a3ae06c168ad0a4a034b8b7143f
velokastalyan <velokastalyan@gmail.com>
2025-12-12 19:29:30 +0300

    Add dev error visibility and logging

--- includes/app.php
+++ includes/app.php
@@ -147,26 +147,73 @@ class CApp extends CApplication
 
         $DebugLevel = 0;
 
-	    $client_ip = ( !empty($HTTP_SERVER_VARS['REMOTE_ADDR']) ) ? $HTTP_SERVER_VARS['REMOTE_ADDR'] : ( ( !empty($HTTP_ENV_VARS['REMOTE_ADDR']) ) ? $HTTP_ENV_VARS['REMOTE_ADDR'] : getenv('REMOTE_ADDR') );
-	    //$this->Session->session_notify($client_ip);
+            $client_ip = ( !empty($HTTP_SERVER_VARS['REMOTE_ADDR']) ) ? $HTTP_SERVER_VARS['REMOTE_ADDR'] : ( ( !empty($HTTP_ENV_VARS['REMOTE_ADDR']) ) ? $HTTP_ENV_VARS['REMOTE_ADDR'] : getenv('REMOTE_ADDR') );
+            //$this->Session->session_notify($client_ip);
 
-		$this->tv['copyright_year'] = date('Y');
-		$this->tv['last_modified'] = gmdate('D, d M Y 00:00:00', time() - 24*60*60) . ' GMT';
-		$this->tv['is_debug_mode'] = ($DebugLevel == 0);
-		$this->tv['site_name'] = $SiteName;
-		
-		return true;
+                $this->tv['copyright_year'] = date('Y');
+                $this->tv['last_modified'] = gmdate('D, d M Y 00:00:00', time() - 24*60*60) . ' GMT';
+                $this->tv['is_debug_mode'] = ($DebugLevel == 0);
+                $this->tv['site_name'] = $SiteName;
+
+                return true;
+    }
+}
+
+function dev_log_error($message) {
+    if (defined('DEV_ERROR_LOG_PATH')) {
+        @error_log($message . "\n", 3, DEV_ERROR_LOG_PATH);
+    } else {
+        @error_log($message);
     }
 }
 
 function on_php_error($code, $message, $filename='', $linenumber=-1, $context=array()) {
-    if (intval($code) != 2048)
-    {
-      //  system_die('Error '.$code.' ('.$message.') occured in '.$filename.' at '.$linenumber.'');
+    if (!IS_DEV_ENVIRONMENT) {
+        return false;
+    }
+
+    $logMessage = sprintf('PHP Error [%s]: %s in %s on line %s', $code, $message, $filename, $linenumber);
+    dev_log_error($logMessage);
+
+    if (!headers_sent()) {
+        echo '<pre style="color:#900">' . htmlspecialchars($logMessage) . '</pre>';
+    }
+
+    return false;
+}
+
+function on_exception($exception) {
+    $logMessage = 'Uncaught exception ' . get_class($exception) . ': ' . $exception->getMessage() . ' in ' . $exception->getFile() . ' on line ' . $exception->getLine();
+    if ($exception->getTraceAsString()) {
+        $logMessage .= "\nStack trace:\n" . $exception->getTraceAsString();
+    }
+
+    if (IS_DEV_ENVIRONMENT) {
+        dev_log_error($logMessage);
+        if (!headers_sent()) {
+            echo '<pre style="color:#900">' . nl2br(htmlspecialchars($logMessage)) . '</pre>';
+        }
+    } else {
+        dev_log_error($logMessage);
     }
 }
+
+function on_php_shutdown() {
+    $error = error_get_last();
+    if ($error && in_array($error['type'], array(E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR, E_USER_ERROR))) {
+        $logMessage = 'Fatal error: ' . $error['message'] . ' in ' . $error['file'] . ' on line ' . $error['line'];
+        dev_log_error($logMessage);
+
+        if (IS_DEV_ENVIRONMENT && !headers_sent()) {
+            echo '<pre style="color:#900">' . htmlspecialchars($logMessage) . '</pre>';
+        }
+    }
+}
+
 @ob_start();
 @set_error_handler('on_php_error');
+@set_exception_handler('on_exception');
+@register_shutdown_function('on_php_shutdown');
 @session_start();
 $GLOBALS['app'] = new CApp();
 ?>
